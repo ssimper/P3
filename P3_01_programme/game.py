e702d2c91654bfac6410c0bd84d63b3e14b8e68e
ssimper <steph.simper@gmail.com>
2020-04-27 12:45:03 +0200

    Mise en place du dossier jeu

---
+++ P3_01_programme/game.py
@@ -0,0 +1,229 @@
+import pygame
+
+from labyrinth import Labyrinth, MacGyver, TitleScore, Score
+from colors import BLACK
+from dimensions import (
+    SPRITE_SIZE_X,
+    SPRITE_SIZE_Y,
+    BOARD_SIZE_X,
+    BOARD_SIZE_Y
+)
+
+
+class HeroSprite(pygame.sprite.Sprite):
+    """Sprite of MacGyver."""
+
+    def __init__(self, hero):
+        super().__init__()
+        self.hero = hero
+        self.image = pygame.image.load(
+            f"pics/mg_{self.hero.profil}.png"
+        ).convert_alpha()
+        self.rect = self.image.get_rect()
+
+        self.update()
+
+    def update(self):
+        """Update the sprite image after a move.
+
+        The representation of MacGyver change with the direction.
+        Specific sprites for left, right and up.
+        """
+        self.image = pygame.image.load(
+            f"pics/mg_{self.hero.profil}.png"
+        ).convert_alpha()
+        x, y = self.hero.position.posx, self.hero.position.posy
+        self.rect.x = x * SPRITE_SIZE_X
+        self.rect.y = y * SPRITE_SIZE_Y
+
+
+class AccessorySprite(pygame.sprite.Sprite):
+    """Sprites representing the three accessories."""
+
+    def __init__(self, lab, accessory_position, image_name):
+        super().__init__()
+        self.lab = lab
+        self.accessory_position = accessory_position
+        self.image = pygame.image.load(
+            f"pics/{image_name}.png").convert_alpha()
+        self.rect = self.image.get_rect()
+
+        self.update()
+
+    def update(self):
+        """Update the sprite.
+
+        If MacGyver takes an object the image is replaced by the path.
+        """
+        x, y = self.accessory_position.posx, self.accessory_position.posy
+        if self.accessory_position in self.lab.rand_pos:
+            self.rect.x = x * SPRITE_SIZE_X
+            self.rect.y = y * SPRITE_SIZE_Y
+        else:
+            self.image = pygame.image.load("pics/path50.png").convert()
+
+
+class TitleScoreSprite(pygame.sprite.Sprite):
+    """Sprite for the infomrations of scoring and status.
+
+    Those informations are : objects to pick-up, you win or you loose.
+    """
+
+    def __init__(self, hero, title_score):
+        super().__init__()
+        self.hero = hero
+        self.title_score = title_score
+        self.image = pygame.image.load(
+            f"pics/{self.hero.destiny}.png").convert()
+        self.rect = self.image.get_rect()
+
+        self.update()
+
+    def update(self):
+        """Updte the sprite.
+
+        If the gamer wins or loses, the sprite coords change to be centered.
+        """
+        self.image = pygame.image.load(
+            f"pics/{self.hero.destiny}.png").convert()
+        x, y = self.title_score.position
+        if self.hero.destiny == "win" or self.hero.destiny == "loose":
+            self.rect.x = 0
+            self.rect.y = 750
+        else:
+            self.rect.x = x * SPRITE_SIZE_X
+            self.rect.y = y * SPRITE_SIZE_Y
+
+
+class ScoreSprite(pygame.sprite.Sprite):
+    """Sprite representing the number of accessories left."""
+
+    def __init__(self, hero, score):
+        super().__init__()
+        self.hero = hero
+        self.score = score
+        self.image = pygame.image.load(
+            f"pics/{self.hero.accessory_number}.png").convert_alpha()
+        self.rect = self.image.get_rect()
+
+        self.update()
+
+    def update(self):
+        """Update the sprite."""
+        self.image = pygame.image.load(
+            f"pics/{self.hero.accessory_number}.png").convert()
+        x, y = self.score.position
+        self.rect.x = x * SPRITE_SIZE_X
+        self.rect.y = y * SPRITE_SIZE_Y
+
+
+class Game:
+    """Creation of the maze.
+
+    Creation of the window containing the game.
+    Instantiation of the labyrinth, the hero, the title barre and the score.
+    Calling the functions to build the map, create random position for the
+    accessories, the start and eding position.
+    """
+
+    def __init__(self):
+        pygame.init()
+        # The labyrinth
+        self.lab = Labyrinth()
+        # Map construction
+        self.lab.build_map("maze.txt")
+        # Random positions for the accessories
+        self.lab.random_position()
+        # Replace the start en finish position in the path
+        self.lab.start_finish_to_path()
+        # Creating the Hero
+        self.hero = MacGyver(self.lab)
+        # Creating the information title
+        self.title_score = TitleScore()
+        # Creating the score countdown
+        self.score = Score()
+        # The windows of the game
+        self.window = pygame.display.set_mode(
+            (BOARD_SIZE_X, BOARD_SIZE_Y)
+        )
+        self.window.fill(BLACK)
+        self.background = pygame.Surface((750, 750))
+        self.background.fill(BLACK)
+        # Walls positioning
+        self.wall = pygame.image.load("pics/wall50.png").convert()
+        for walls in self.lab.walls:
+            self.background.blit(
+                self.wall,
+                (walls.posx * SPRITE_SIZE_X, walls.posy * SPRITE_SIZE_Y)
+            )
+        # Paths positioning
+        self.path = pygame.image.load("pics/path50.png").convert()
+        for paths in self.lab.paths:
+            self.background.blit(
+                self.path,
+                (paths.posx * SPRITE_SIZE_X, paths.posy * SPRITE_SIZE_Y)
+            )
+        # Starting point positioning
+        self.start_point = pygame.image.load("pics/start50.png").convert()
+        self.background.blit(
+            self.start_point,
+            (
+                self.lab.start_pos.posx * SPRITE_SIZE_X,
+                self.lab.start_pos.posy * SPRITE_SIZE_Y
+            )
+        )
+        # Finish point positioning
+        self.finish_point = pygame.image.load("pics/finish50.png").convert()
+        self.background.blit(
+            self.finish_point,
+            (
+                self.lab.finish_pos.posx * SPRITE_SIZE_X,
+                self.lab.finish_pos.posy * SPRITE_SIZE_Y
+            )
+        )
+        # Coordinate assignment to the accessories.
+        gel_position, ether_position, syringe_position = self.lab.rand_pos
+        # Creation of the sprites group
+        self.sprites = pygame.sprite.Group(
+            AccessorySprite(self.lab, gel_position, "tub"),
+            AccessorySprite(self.lab, ether_position, "ether"),
+            AccessorySprite(self.lab, syringe_position, "syringe"),
+            HeroSprite(self.hero),
+            ScoreSprite(self.hero, self.score),
+            TitleScoreSprite(self.hero, self.title_score)
+        )
+
+    def start(self):
+        """Starting the game !"""
+        self.running = True
+        while self.running:
+            self.window.blit(self.background, (0, 0))
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    self.running = False
+                # Waiting for left, right, up and down keys as long as MacGyver
+                # hasn't reached the guard yet.
+                elif event.type == pygame.KEYDOWN and self.hero.status:
+                    if event.key == pygame.K_RIGHT:
+                        self.hero.change_position("right")
+                    elif event.key == pygame.K_LEFT:
+                        self.hero.change_position("left")
+                    elif event.key == pygame.K_UP:
+                        self.hero.change_position("up")
+                    elif event.key == pygame.K_DOWN:
+                        self.hero.change_position("down")
+
+            self.sprites.update()
+            self.sprites.draw(self.window)
+
+            pygame.display.update()
+
+
+def main():
+    """Instantiation and starting the game."""
+    game = Game()
+    game.start()
+
+
+if __name__ == "__main__":
+    main()
---
+++ P3_01_programme/labyrinth.py
@@ -0,0 +1,138 @@
+import random
+
+
+class Labyrinth:
+    """Building the maze.
+
+    The construction is made from a txt file.
+Each '.' representing a path and each '#' representing a wall.
+Pick 3 random position for the accessories MacGyver needs to win.
+Positioning of the strat and end points of the game.
+Creation of MacGyver, the information title and the score.
+    """
+
+    def __init__(self):
+        self.paths = set()  # set of paths
+        self.rand_pos = set()  # set of items
+        self.walls = set()  # set of walls
+        self.size_x = 0  # board width
+        self.size_y = 0  # board height
+
+    def build_map(self, the_file):
+        """Function that reads the file, generate a set of paths and
+         a set of walls, find the size of the labyrinth, find the start
+         and finish point of the labyrinth"""
+        with open(the_file, "r") as f:
+            # reading each lines of the file
+            for num_lines, lines in enumerate(f):
+                num_col = 0
+                # read each caracters of lines
+                for caracters in lines.strip():
+                    if caracters == ".":  # a path
+                        self.paths.add(Position(num_col, num_lines))
+                    elif caracters == "#":  # a wall
+                        self.walls.add(Position(num_col, num_lines))
+                    elif caracters == "S":  # the start position
+                        self.start_pos = Position(num_col, num_lines)
+                    elif caracters == "F":  # the finish position
+                        self.finish_pos = Position(num_col, num_lines)
+                    num_col += 1
+            self.size_y = num_lines + 1
+            self.size_x = num_col
+            return self.walls, self.paths, self.size_x, self.size_y,\
+                self.start_pos, self.finish_pos
+
+    def start_finish_to_path(self):
+        """Put the start and the finish position in the paths set"""
+        self.paths.add(self.start_pos)
+        self.paths.add(self.finish_pos)
+
+    def random_position(self):
+        '''Loop throught self_path, 3 times randomly pickup coords
+        and add it to self.rand_path set '''
+        self.rand_pos.clear()
+        self.rand_pos = random.sample(self.paths, 3)
+        return self.rand_pos
+
+
+class Position:
+    """Create and compare positions."""
+
+    def __init__(self, posx, posy):
+        self.posx = posx
+        self.posy = posy
+
+    def __eq__(self, other):
+        """Special method to compare two positions."""
+        return (
+            self.posx == other.posx
+            and self.posy == other.posy
+        )
+
+    def __hash__(self):
+        """Keep the hashability for the set."""
+        return hash((self.posx, self.posy))
+
+
+class TitleScore:
+    """Title position."""
+
+    def __init__(self):
+        self.position = (3, 15)
+
+
+class Score:
+    """Score Position"""
+
+    def __init__(self):
+        self.position = (10, 15)
+
+
+class MacGyver:
+    """Creating the hero."""
+
+    def __init__(self, lab):
+        # the first position of MG is the start point
+        self.position = lab.start_pos
+        # Connector to the Labyrinth's class.
+        self.lab = lab
+        # Number of accessories to pick-up.
+        self.accessory_number = len(lab.rand_pos)
+        # Information to show to the gamer.
+        self.destiny = "title_score"
+        # Default representation of the MacGyver sprite.
+        self.profil = "right"
+        # As long as MacGyver hasn't reach the guard.
+        self.status = True
+
+    def change_position(self, direction):
+        """Calculate the new MacGyver position from the keyboard input
+        and determines whether it is in the path list or the wall list.
+        It also verify if the new position is the same as an accessory."""
+        new_position = Position(self.position.posx, self.position.posy)
+        if direction == "right":
+            self.profil = "right"
+            new_position.posx = self.position.posx + 1
+        elif direction == "left":
+            self.profil = "left"
+            new_position.posx = self.position.posx - 1
+        elif direction == "up":
+            self.profil = "up"
+            new_position.posy = self.position.posy - 1
+        elif direction == "down":
+            new_position.posy = self.position.posy + 1
+        # Verify if the new position is an accessory
+        if new_position in self.lab.rand_pos:
+            self.accessory_number -= 1
+            # delete picked-up accessory from set
+            self.lab.rand_pos.remove(new_position)
+        if new_position in self.lab.paths:
+            self.position = new_position
+        if new_position == self.lab.finish_pos:
+            if self.accessory_number == 0:
+                self.profil = self.destiny = "win"
+            else:
+                print("You loose !!!")
+                self.profil = self.destiny = "loose"
+            self.status = False
+        return self.position, self.profil
